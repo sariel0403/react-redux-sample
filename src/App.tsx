d94b6b36cd924b3746ea3eb91a1a0cdb7d8517f2
popcorn412 <felix44291@gmail.com>
2022-11-15 22:28:05 +0900

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import useStore from "./useStore";
 
 function App() {
+  const { theme, update } = useStore();
+
+  React.useEffect(() => {
+    update({ theme: "123231" });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
